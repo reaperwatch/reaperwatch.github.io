c2b1127cd49dc527f99ec1b0942b5fbf0386163f
reaperwatch <ofirzadka1@gmail.com>
2026-05-02 00:01:02 +0300

    gdfgfdg

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
-/*self.addEventListener("install", (event) => {
+self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(fetch(event.request));
 });
-*/
